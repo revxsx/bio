bc4a90ffef37f0e78f2e2d915c41919c2de4e3c6
rev\x <Nepana73@mail.ru>
2025-03-21 17:15:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,33 +4,41 @@ const bioText = document.querySelector('.bio');
 
 let isEnglish = true;
 
-languageBtn.addEventListener('click', () => {
+languageBtn.addEventListener('click', () => {
+
     hiText.classList.add('fade-out');
     bioText.classList.add('fade-out');
-
+
+
     setTimeout(() => {
-        if (isEnglish) {
+        if (isEnglish) {
+
             hiText.textContent = 'Привет, я rev\\x';
             bioText.textContent = 'пытаюсь программировать, пытаюсь стать лучше / / /';
-            languageBtn.textContent = 'RU';
-        } else {
+            languageBtn.textContent = 'en';
+        } else {
+
             hiText.textContent = 'Hi, I\'m rev\\x';
             bioText.textContent = 'trying to program, trying to get better / / /';
-            languageBtn.textContent = 'EN';
+            languageBtn.textContent = 'ru';
         }
 
         
         hiText.classList.remove('fade-out');
         bioText.classList.remove('fade-out');
-
+
+
         hiText.style.animation = 'none';
-        bioText.style.animation = 'none'; 
+        bioText.style.animation = 'none';
+ 
         void hiText.offsetWidth;
         void bioText.offsetWidth;
-
+
+
         hiText.style.animation = 'fadeIn 0.5s ease forwards';
         bioText.style.animation = 'fadeIn 0.5s ease forwards';
-
+
+
         isEnglish = !isEnglish;
     }, 500); // Задержка соответствует длительности анимации
 });
